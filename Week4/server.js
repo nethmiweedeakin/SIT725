4a84019f4b726abcc07da070192eed8c63d23262
nethmiweedeakin <s224350062@deakin.edu.au>
2025-03-27 20:27:44 +1100

    Committed week3

---
+++ Week4/server.js
@@ -0,0 +1,34 @@
+var express = require("express")
+var app = express()
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const cardList = [
+{
+title: "Kitten 2",
+image: "images/kitten-2.jpg",
+link: "About Kitten 2",
+desciption: "Demo desciption about kitten 2"
+},
+{
+title: "Kitten 3",
+image: "images/kitten-3.jpg",
+link: "About Kitten 3",
+desciption: "Demo desciption about kitten 3"
+}
+]
+app.get('/api/projects',(req,res) => {
+res.json({statusCode: 200, data: cardList, message:"Success"})
+})
+var port = process.env.port || 5500;
+app.listen(port,()=>{
+console.log("App listening to: "+port)
+})
+
+const getProjects = () => {
+  $.get('/api/projects',(response) => {
+  if(response.statusCode==200){
+  addCards(response.data);
+  }
+  })
+  }  
