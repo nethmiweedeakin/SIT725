23221070a27fd7199b8b8bfc67d1c811f5eed512
nethmiweedeakin <s224350062@deakin.edu.au>
2025-04-03 17:04:55 +1100

    Added week 4

--- Week4/server.js
+++ Week4/server.js
@@ -1,34 +1,36 @@
 var express = require("express")
 var app = express()
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const cardList = [
-{
-title: "Kitten 2",
-image: "images/kitten-2.jpg",
-link: "About Kitten 2",
-desciption: "Demo desciption about kitten 2"
-},
-{
-title: "Kitten 3",
-image: "images/kitten-3.jpg",
-link: "About Kitten 3",
-desciption: "Demo desciption about kitten 3"
-}
-]
-app.get('/api/projects',(req,res) => {
-res.json({statusCode: 200, data: cardList, message:"Success"})
-})
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    });
+    mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+    });
+
+    const ProjectSchema = new mongoose.Schema({
+        title: String,
+        image: String,
+        link: String,
+        description: String,
+        });
+        const Project = mongoose.model('Project', ProjectSchema);
+
+        app.get('/api/projects', async (req, res) => {
+            const projects = await Project.find({});
+            res.json({ statusCode: 200, data: projects, message: 'Success' });
+            });
+
 var port = process.env.port || 5500;
 app.listen(port,()=>{
 console.log("App listening to: "+port)
 })
 
-const getProjects = () => {
-  $.get('/api/projects',(response) => {
-  if(response.statusCode==200){
-  addCards(response.data);
-  }
-  })
-  }  
+module.exports = { Project };
+// Export the Project model for use in other files
